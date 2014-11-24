9d9b6cffd16d73642d42afb1cd72a06f33786c09
Nickolai Novik <is.infinity@yahoo.com>
2014-11-25 01:09:00 +0200

    basic example for readme

---
+++ examples/simple.py
@@ -0,0 +1,20 @@
+import asyncio
+import aioredis
+from aioredis_bloom import BloomFilter
+
+
+loop = asyncio.get_event_loop()
+
+
+@asyncio.coroutine
+def go():
+    redis = yield from aioredis.create_redis(
+        ('localhost', 6379), loop=loop)
+    bloom = BloomFilter(redis, 'mybloom', 100000, 0.0001)
+    yield from bloom.add('python')
+    yield from bloom.add('asyncio')
+    result = yield from bloom.contains('tornado')
+    print(result)
+
+    redis.close()
+loop.run_until_complete(go())

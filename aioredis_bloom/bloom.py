8a66c7e25076b56a3b207b49a85c9504fc9d3ec5
Nickolai Novik <is.infinity@yahoo.com>
2014-11-26 22:03:31 +0200

    add intersection and union support

--- aioredis_bloom/bloom.py
+++ aioredis_bloom/bloom.py
@@ -1,22 +1,29 @@
 import math
 import mmh3
 import asyncio
+import uuid
 
 
 class BloomFilter(object):
 
-    def __init__(self, redis, key_prefix, capacity, error_rate):
+    def __init__(self, redis, capacity=100000, error_rate=0.001,
+                 redis_key=None):
         # redis settings
         self._conn = redis
-        self._redis_key = '{}:capacity:{}:error_rate:{}'.format(
-            key_prefix, capacity, error_rate)
+        # if redis key not provided, generate random one
+        self._redis_key = redis_key or 'bloom:{}:cap:{}:err:{}'.format(
+            uuid.uuid4().hex, redis_key, capacity, error_rate)
 
         self._capacity = capacity
-        self._failure_rate = error_rate
+        self._error_rate = error_rate
         # bloom filter settings
         self._filter_size, self._hash_funcs = self._optimal_bloom_filter(
-            self._capacity, self._failure_rate)
+            self._capacity, self._error_rate)
         self._bits_per_slice = self._filter_size/self._hash_funcs
+
+    @property
+    def redis_key(self):
+        return self._redis_key
 
     @property
     def capacity(self):
@@ -27,6 +34,69 @@ class BloomFilter(object):
     def error_rate(self):
         """Expected error rate for Bloom filter"""
         return self._error_rate
+
+    @asyncio.coroutine
+    def add(self, key):
+        """
+
+        :param key:
+        :return:
+        """
+        bit_positions = self._calc_bit_positions(key)
+        yield from self._set_bits(self._redis_key, bit_positions)
+
+    @asyncio.coroutine
+    def contains(self, key):
+        """
+
+        :param key:
+        :return:
+        """
+        bit_positions = self._calc_bit_positions(key)
+        yield from self._check_bits(self._redis_key, bit_positions)
+
+    @asyncio.coroutine
+    def union(self, other_bloom, redis_key):
+        """
+
+        :param other_bloom:
+        :param redis_key:
+        :return:
+        """
+        self._validate_bloom_input(other_bloom)
+        if not redis_key:
+            redis_key = 'bloom:intersection:{}:{}'.format(
+                self.redis_key, other_bloom.redis_key)
+
+        capacity = other_bloom.capacity
+        error_rate = other_bloom.error_rate
+
+        yield from self._conn.bitop_or(
+            redis_key, self.redis_key, other_bloom.redis_key)
+        new_bloom = BloomFilter(capacity, error_rate, redis_key)
+        return new_bloom
+
+
+    @asyncio.coroutine
+    def intersection(self, other_bloom, redis_key=None):
+        """
+
+        :param other_bloom:
+        :param redis_key:
+        :return:
+        """
+        self._validate_bloom_input(other_bloom)
+        if not redis_key:
+            redis_key = 'bloom:intersection:{}:{}'.format(
+                self.redis_key, other_bloom.redis_key)
+
+        capacity = other_bloom.capacity
+        error_rate = other_bloom.error_rate
+
+        yield from self._conn.bitop_and(
+            redis_key, self.redis_key, other_bloom.redis_key)
+        new_bloom = BloomFilter(capacity, error_rate, redis_key)
+        return new_bloom
 
     @staticmethod
     def _optimal_bloom_filter(capacity, failure_rate):
@@ -79,33 +149,7 @@ class BloomFilter(object):
         """
         yield from self._conn.eval(script_check_bits, [key], bit_positions)
 
-    @asyncio.coroutine
-    def add(self, key):
-        """
-
-        :param key:
-        :return:
-        """
-        bit_positions = self._calc_bit_positions(key)
-        yield from self._set_bits(self._redis_key, bit_positions)
-
-    @asyncio.coroutine
-    def contains(self, key):
-        """
-
-        :param key:
-        :return:
-        """
-        bit_positions = self._calc_bit_positions(key)
-        yield from self._check_bits(self._redis_key, bit_positions)
-
-    @asyncio.coroutine
-    def union(self, other_bloom):
-        """
-
-        :param other_bloom:
-        :return:
-        """
+    def _validate_bloom_input(self, other_bloom):
         # return new_bloom
         if not isinstance(other_bloom, BloomFilter):
             raise TypeError('other_bloom must be instance of {}'
@@ -116,13 +160,3 @@ class BloomFilter(object):
 
         if self.error_rate != other_bloom.error_rate:
             raise ValueError('Error rate of both bloom filters must be equal')
-        raise NotImplementedError
-
-    def intersection(self, other_bloom):
-        """
-
-        :param other_bloom:
-        :return:
-        """
-        # return new_bloom
-        raise NotImplementedError
